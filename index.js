6813423aac80175e4ce6bcdf54f65d739ba0a4e5
hellojaeyoon <hellojaeyoon.lee@navercorp.com>
2023-12-18 14:28:33 +0900

    aa

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const core = require('@actions/core');
 try {
 	const token = core.getInput('GIT_TOKEN');
 	core.setOutput('token', token);
-	print(token);
+	console.log(token);
 
 } catch (error) {
 	core.setFailed(error.message);
