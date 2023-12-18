ca2c78897901c5b8abab909b3b66f8e353269fc7
hellojaeyoon <hellojaeyoon.lee@navercorp.com>
2023-12-18 14:43:12 +0900

    testpr

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ try {
 	const token = core.getInput('GIT_TOKEN');
 	core.setOutput('token', token);
 	console.log(token);
+	const pr_title = core.getInput('pr_title');
+	console.log(pr_title);
 
 } catch (error) {
 	core.setFailed(error.message);
