0872567f0bc7b78cca58ce938981f65844c9d709
hellojaeyoon <68373518+hellojaeyoon@users.noreply.github.com>
2023-12-18 18:56:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -20,20 +20,26 @@ const webhook = new IncomingWebhook(url);
 		console.log(pullRequestUrl.type);
 		const textExample = `${pullRequestUrl}`;
 	  	await webhook.send({
-	    	text: textExample,
+	    	text: pr_title,
 	        attachments:[
 		      {
-			 "fallback":"PR을 확인해주세요~: <`${pullRequestUrl}`|PR>",
-			 "pretext":"PR을 확인해주세요~: <`${pullRequestUrl}`|PR>",
-			 "color":"#D00000",
-			 "fields":[
-			    {
-			       "title":"Notes",
-			       "value":"This is much easier than I thought it would be.",
-			       "short":false
-			    }
-			 ]
-			}
+		          "fallback": "요청에 실패했습니다ㅜ",
+		          "color": "#2eb886",
+		          "pretext": "PR이 당신의 리뷰를 기다리고 있어요!",
+		          "author_name": "roseline",
+		          "author_link": "https://github.com/roseline124",
+		          "author_icon": "https://avatars.githubusercontent.com/u/41788121?s=460&u=ee6a6f6499aa68a23947cfb76d5e9cb6eebfd29c&v=4",
+		          "title": pr_title,
+		          "title_link": textExample,
+		          "text": "피알내용조금?",
+		          "fields": [
+		              {
+		                  "title": "썸띵",
+		                  "value": "썸밸류",
+		                  "short": false
+		              }
+		          ]
+		      }
 		   ]
 	 	});
 	} catch (error) {
