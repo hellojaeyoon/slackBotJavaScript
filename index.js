a6d78908ac1ed0983df9842d330513d80cdf1b2a
hellojaeyoon <68373518+hellojaeyoon@users.noreply.github.com>
2023-12-18 20:20:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const { IncomingWebhook } = require('@slack/webhook');
 const url = core.getInput('WEBHOOK_URL');
 const webhook = new IncomingWebhook(url);
 
+const NOTI_COLORS = {
+	"[D-0]" : "#FF0000",
+	"[D-1]" : "#FF0000",
+	"[D-2]" : "#FFA500",
+	"[D-3]" : "#FFA500",
+	"[D-4]" : "#008000",
+	"[D-5]" : "#008000",
+}
+
 // Send the notification
 (async () => {
 
@@ -21,12 +30,18 @@ const webhook = new IncomingWebhook(url);
 		console.log(pullRequestUrl);
 		console.log(pullRequestUrl.type);
 		const textExample = `${pullRequestUrl}`;
+
+		const dueDate = pr_title.substr(0, 5);
+		console.log(dueDate);
+		const color = NOTI_COLORS[`${dueDate}`];
+  		console.log(color);
+		
 	  	await webhook.send({
 	    	text: `${open_issues_count}개의 PR이 여러분들의 관심을 기다리고 있어요~`,
 	        attachments:[
 		      {
 		          "fallback": "요청에 실패했습니다ㅜ",
-		          "color": "#2eb886",
+		          "color": color,
 		          "pretext": "PR이 당신의 리뷰를 기다리고 있어요!",
 		          "author_name": "hellojaeyoon",
 		          "author_link": "https://github.com/hellojaeyoon",
