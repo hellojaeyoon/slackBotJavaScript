1bb176dea735690981ebf9f0db1ca4f0c37ec0e8
hellojaeyoon <hellojaeyoon.lee@navercorp.com>
2023-12-18 16:33:46 +0900

    test

--- index.js
+++ index.js
@@ -6,23 +6,27 @@ const webhook = new IncomingWebhook(url);
 
 // Send the notification
 (async () => {
-  await webhook.send({
-    text: 'hellojaeyoon...',
-  });
+
+	try {
+		const token = core.getInput('GIT_TOKEN');
+		core.setOutput('token', token);
+		console.log(token);
+		const pr_title = core.getInput('pr_title');
+		console.log(pr_title);
+		const githubcontext = core.getInput('GITHUB_CONTEXT');
+		console.log(githubcontext);
+		const pullRequestUrl = core.getInput('GITHUB_PR_URL');
+		console.log(pullRequestUrl);
+		const text = "prmessage";
+		text.link(pullRequestUrl)
+	  	await webhook.send({
+	    	text: text,
+	    	t
+	 	 });
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+	
 })();
 
-try {
-	const token = core.getInput('GIT_TOKEN');
-	core.setOutput('token', token);
-	console.log(token);
-	const pr_title = core.getInput('pr_title');
-	console.log(pr_title);
-	const githubcontext = core.getInput('GITHUB_CONTEXT');
-	console.log(githubcontext);
-	const pullRequestUrl = githubcontext.event.pull_request.html_url;
-	console.log(pullRequestUrl);
-
-} catch (error) {
-	core.setFailed(error.message);
-}
 
