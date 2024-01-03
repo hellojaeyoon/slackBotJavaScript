1cd51c1972f469ba948deacd09df06670104d795
hellojaeyoon <68373518+hellojaeyoon@users.noreply.github.com>
2024-01-03 12:45:41 +0900

    Update api.js

--- api.js
+++ api.js
@@ -1,17 +1,33 @@
-import * as core from "@actions/core";
-import * as github from "@actions/github";
 
-export const initialize = () => {
-  global.owner = github.context.repo.owner;
-  global.repo = github.context.repo.repo;
+const fetchAllPages = async (
+		  request,
+		  params,
+		  maxCount = Number.POSITIVE_INFINITY
+		) => {
+		  let [page, len, count] = [1, 0, 0];
+		  const result = [];
+		
+		  do {
+		    const { data } = await request({ ...params, per_page: 100, page });
+		    for (var i = 0; i < data.length; i++) {
+			  var object = data[i];
+			  const pullRequest = new PullRequest(object.title, object.html_url, object.head.ref, object.base.ref, object.created_at, object.updated_at); 
+			  result.push(pullRequest);
+		    }
+		    console.log(`data`);
+		    console.log(data);
+		
+		    [page, len, count] = [page + 1, data.length, count + data.length];
+		  } while (len === 100 && count < maxCount);
+		  console.log(`result`);
+		  console.log(result)
+		  return result;
+		};
 
-  const githubToken = core.getInput("GH_TOKEN", { required: true });
-  if (!githubToken) {
-    throw new Error("Missing GH_TOKEN environment variable");
-  }
-  const octokit = github.getOctokit(githubToken);
-
-  global.octokit = octokit;
-
-  console.log(`Successfully initialized.`);
+export const getPRList = async () => {
+  return fetchAllPages(global.octokit.rest.pulls.list, {
+    owner: global.owner,
+    repo: global.repo,
+    state: "open",
+  });
 };
