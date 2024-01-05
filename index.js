7a3561625927723e0cac120f755144950c272796
hellojaeyoon <68373518+hellojaeyoon@users.noreply.github.com>
2024-01-05 14:57:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,64 +2,7 @@ import * as core from "@actions/core";
 import * as github from "@actions/github";
 import { initialize } from "./initialize.js";
 import { listOfPRs } from "./api.js";
-import { time_ago } from "./SlackMessage.js";
-
-const NOTI_COLORS = {
-  "[D-0]": "#FF0000",
-  "[D-1]": "#FF0000",
-  "[D-2]": "#FFA500",
-  "[D-3]": "#FFA500",
-  "[D-4]": "#008000",
-  "[D-5]": "#008000",
-};
-
-// function time_ago(time) {
-//   var time_formats = [
-//     [60, "seconds", 1], // 60
-//     [120, "1 minute ago", "1 minute from now"], // 60*2
-//     [3600, "minutes", 60], // 60*60, 60
-//     [7200, "1 hour ago", "1 hour from now"], // 60*60*2
-//     [86400, "hours", 3600], // 60*60*24, 60*60
-//     [172800, "Yesterday", "Tomorrow"], // 60*60*24*2
-//     [604800, "days", 86400], // 60*60*24*7, 60*60*24
-//     [1209600, "Last week", "Next week"], // 60*60*24*7*4*2
-//     [2419200, "weeks", 604800], // 60*60*24*7*4, 60*60*24*7
-//     [4838400, "Last month", "Next month"], // 60*60*24*7*4*2
-//     [29030400, "months", 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
-//     [58060800, "Last year", "Next year"], // 60*60*24*7*4*12*2
-//     [2903040000, "years", 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
-//     [5806080000, "Last century", "Next century"], // 60*60*24*7*4*12*100*2
-//     [58060800000, "centuries", 2903040000], // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
-//   ];
-//   var seconds = (+new Date() - time) / 1000,
-//     token = "ago",
-//     list_choice = 1;
-
-//   if (seconds == 0) {
-//     return "Just now";
-//   }
-//   if (seconds < 0) {
-//     seconds = Math.abs(seconds);
-//     token = "from now";
-//     list_choice = 2;
-//   }
-//   var i = 0,
-//     format;
-//   while ((format = time_formats[i++]))
-//     if (seconds < format[0]) {
-//       if (typeof format[2] == "string")
-//         return format[list_choice];
-//       else
-//         return (
-//           Math.floor(seconds / format[2]) +
-//           " " +
-//           format[1] +
-//           " " +
-//           token
-//         );
-//     }
-//   return time;
-// }
+import { time_ago, NOTI_COLORS } from "./SlackMessage.js";
 
 var attachments = [];
 
